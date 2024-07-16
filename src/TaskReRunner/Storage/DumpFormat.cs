8a2ca9f8d73aa4cd48a6db6d8fb538ab5f5531f4
ereali-aneo <ereali@aneo.fr>
2024-07-16 17:51:50 +0200

    style: use PR patch to format

--- src/TaskReRunner/Storage/DumpFormat.cs
+++ src/TaskReRunner/Storage/DumpFormat.cs
@@ -14,12 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using ArmoniK.Api.gRPC.V1;
 
